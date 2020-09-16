d229a650cfb26b9834b76dcc5334d302887853c3
Crowdbotics <team@crowdbotics.com>
2020-09-16 16:23:23 +0000

    Update components

--- src/features/BlankScreen910806/screens/blank.js
+++ src/features/BlankScreen910806/screens/blank.js
@@ -214,41 +214,77 @@ export class _Blank extends React.Component {
       >
         Sample text content
       </Text>
-      <Icon
-        iconFont="Eva Design Icons"
-        name="star"
-        height={20}
-        width={20}
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontFamily: "Roboto-Regular",
-          textAlign: "left",
-          textAlignVertical: "center",
-          textDecorationLine: "none",
-          textTransform: "none",
-          lineHeight: 12,
-          letterSpacing: 0
-        }}
-      />
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen 710804")}
+      >
+        <Icon
+          iconFont="Eva Design Icons"
+          name="star"
+          height={20}
+          width={20}
+          style={{
+            marginLeft: 0,
+            marginRight: 0,
+            marginTop: 5,
+            marginBottom: 5,
+            paddingLeft: 5,
+            paddingRight: 5,
+            paddingTop: 5,
+            paddingBottom: 5,
+            overflow: "visible",
+            borderColor: "#000000",
+            borderStyle: "solid",
+            borderWidth: 0,
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            borderTopWidth: 0,
+            borderBottomWidth: 0,
+            borderRadius: 0,
+            fontSize: 20,
+            color: "#000000",
+            backgroundColor: "#ffffff",
+            fontFamily: "Roboto-Regular",
+            textAlign: "left",
+            textAlignVertical: "center",
+            textDecorationLine: "none",
+            textTransform: "none",
+            lineHeight: 12,
+            letterSpacing: 0
+          }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen 710804")}
+      >
+        <Image
+          resizeMode="cover"
+          source={{
+            uri:
+              "https://d3nfddh2sbo9hk.cloudfront.net/media/project_component_resources/Screen_Shot_2020-09-11_at_10.07.16_AM_Qwt2gFh.png"
+          }}
+          style={{
+            width: "100%",
+            height: 200,
+            marginLeft: 0,
+            marginRight: 0,
+            marginTop: 0,
+            marginBottom: 0,
+            paddingLeft: 0,
+            paddingRight: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+            overflow: "visible",
+            borderColor: "#000000",
+            borderStyle: "solid",
+            borderWidth: 0,
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            borderTopWidth: 0,
+            borderBottomWidth: 0,
+            borderRadius: 0
+          }}
+        />
+      </TouchableOpacity>
     </View>
   )
 }
