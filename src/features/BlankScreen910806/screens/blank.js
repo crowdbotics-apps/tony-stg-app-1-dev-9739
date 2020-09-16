e065cc26cda07cfad4a5522e97a40260576a84ef
Crowdbotics <team@crowdbotics.com>
2020-09-16 06:38:37 +0000

    Update components

--- src/features/BlankScreen910806/screens/blank.js
+++ src/features/BlankScreen910806/screens/blank.js
@@ -105,6 +105,39 @@ export class _Blank extends React.Component {
           Go to Page 1
         </Button>
       </TouchableOpacity>
+      <Text
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          fontSize: 12,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontFamily: "System-Regular",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textAlign: "left",
+          textAlignVertical: "center",
+          textDecorationLine: "none",
+          textTransform: "none",
+          lineHeight: 12,
+          letterSpacing: 0
+        }}
+      >
+        Sample text content
+      </Text>
     </View>
   )
 }
